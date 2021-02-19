fc67372700c5981e0208994f61483f84512265d9
pauliejog <Elakpjd7!>
2021-02-18 17:34:49 -0700

    added comments to shell

--- shell/my_shell.py
+++ shell/my_shell.py
@@ -33,18 +33,18 @@ while 1:
         sys.exit(1)
     elif rc == 0:
         # child executes command
-        for dir in re.split(':', os.environ['PATH']):
+        for dir in re.split(':', os.environ['PATH']):   # try each directory
             program = '%s/%s' % (dir, tkns[0])
 
             try:
-                os.execve(program, tkns, os.environ)
+                os.execve(program, tkns, os.environ)    # try to exec program
             except FileNotFoundError:
-                pass
+                pass                                    # fail quietly
         os.write(2, ('Child:    failed exec %s\n' % tkns[0]).encode())
         sys.exit(1)  # terminate
 
     else:
-        # parent waits for child to terminate
+        # parent waits for child to terminate (forked ok)
         os.write(1, ('Parent: My pid=%d. Child pi=%d\n' %
                      (pid, rc)).encode())
         childPidCode = os.wait()
