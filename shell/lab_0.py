9ed2fb8efeaedde0c7dca823d7d18d6683a94115
pauliejog <Elakpjd7!>
2021-02-23 20:29:10 -0700

    added my_read_lines to lab_0

--- shell/lab_0.py
+++ shell/lab_0.py
@@ -17,17 +17,16 @@ def get_char():
 
     if next_c == limit:
         next_c = 0
-        limit = read(0, 100)
+        limit = read(0, 100)        # allocate bytes
 
         if limit == 0:
-            return 'EOF'
+            return None
 
-    # next_c oob
-    if next_c > len(limit) - 1:
-        return 'EOF'
-
-    ch = chr(limit[next_c])
+    if next_c >= len(limit) - 1:    # check upperbound
+        return None
+    ch = chr(limit[next_c])         # convert to char (from ASCII)
     next_c += 1
+
     return ch
 
 
@@ -38,11 +37,23 @@ def my_read_line():
     ch = get_char()
 
     # get each char of line
-    while(ch != '' and ch != 'EOF'):
+    while(ch != '' and ch != None):  # check character
         line += ch
         ch = get_char()
 
-    next_c = 0
+    next_c = 0                      # reset next_c and limit after line is read
     limit = 0
 
     return line
+
+
+def my_read_lines():
+    num_lines = 0
+    in_line = my_read_lines()       # read line
+
+    while len(in_line):
+        num_lines += 1
+        print(f'###line {num_lines}: <{str(in_line)}> ###\n')
+
+        in_line = my_read_lines()
+        print(f'eof after {num_lines}\n')
